6f2e2a72c016ca9000bf7c1b4f9ada958eb2395c
Vivekanand P V <vivekanandpv@outlook.com>
2024-01-19 13:48:58 +0530

    done

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,39 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted, onUpdated } from "vue";
 
-//  wrapper, type-safe (TS)
-//  recommended for simple types: number, string, boolean
-//  could also work with complex types
-//  auto type inference
+//  https://vuejs.org/api/composition-api-lifecycle
+
+onMounted(() => {
+  console.log("onMounted");
+});
+
 const counter = ref(0);
 
-//  But unwrapping by .value.x is required
 const increment = () => {
   counter.value++;
-}
+};
 
 const decrement = () => {
   counter.value--;
-}
+};
+
+onUpdated(() => {
+  console.log("onUpdated");
+});
 </script>
 
 <template>
   <div class="p-5">
     <h2>Vue 3 Reactivity API</h2>
-    <hr>
-    <!-- bind directly to the wrapper -->
+    <hr />
     <p>Counter: {{ counter }}</p>
 
-    <button class="btn btn-primary me-2" @click="increment">Increment (Setup)</button>
-    <button class="btn btn-primary me-2" @click="decrement">Decrement (Setup)</button>
-
-    <!-- this too will work, but only in the template -->
-    <button class="btn btn-primary me-2" @click="counter--">Decrement (Template Direct)</button>
+    <button class="btn btn-primary me-2" @click="increment">
+      Increment (Setup)
+    </button>
+    <button class="btn btn-primary me-2" @click="decrement">
+      Decrement (Setup)
+    </button>
   </div>
 </template>
 
