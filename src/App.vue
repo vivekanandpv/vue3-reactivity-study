1418793e32ed0f89d2d816b41ab83b4a43c71a95
Vivekanand P V <vivekanandpv@outlook.com>
2024-01-19 13:34:40 +0530

    done

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
-<script setup lang="ts">
+<script lang="ts">
 import { ref } from 'vue';
 
-//  wrapper, type-safe (TS)
-//  recommended for simple types: number, string, boolean
-//  could also work with complex types
-//  auto type inference
-const counter = ref(0);
+export default {
+  setup() {
+    const counter = ref(0);
 
-//  But unwrapping by .value.x is required
-const increment = () => {
-  counter.value++;
-}
+    const increment = () => {
+      counter.value++;
+    }
 
-const decrement = () => {
-  counter.value--;
+    const decrement = () => {
+      counter.value--;
+    }
+
+    return {
+      counter,
+      increment,
+      decrement
+    }
+  },
+  methods: {
+    secondaryIncrement() {
+      //  this.counter is a reactive property now
+      //  It's not a wrapper anymore!
+      this.counter++;
+      // this.increment();  //  this too is possible
+    }
+  }
 }
 </script>
 
@@ -24,11 +37,14 @@ const decrement = () => {
     <!-- bind directly to the wrapper -->
     <p>Counter: {{ counter }}</p>
 
-    <button class="btn btn-primary me-2" @click="increment">increment</button>
-    <button class="btn btn-primary me-2" @click="decrement">Decrement</button>
+    <button class="btn btn-primary me-2" @click="increment">Increment (Setup Export)</button>
+    <button class="btn btn-primary me-2" @click="decrement">Decrement (Setup Export)</button>
 
     <!-- this too will work, but only in the template -->
-    <button class="btn btn-primary me-2" @click="counter--">Decrement</button>
+    <button class="btn btn-primary me-2" @click="counter--">Decrement (Direct Template)</button>
+
+    <!-- composition api way of handling events -->
+    <button class="btn btn-primary me-2" @click="secondaryIncrement">Increment (Composition API)</button>
   </div>
 </template>
 
